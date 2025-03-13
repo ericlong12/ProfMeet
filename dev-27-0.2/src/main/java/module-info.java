9d684c6128f0739654a0fb7ea77a04190587c0e3
Frances Belleza <francesanne.belleza@sjsu.edu>
2025-03-13 16:03:37 -0700

    Deleted extra directory

--- dev-27-0.2/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module s25.cs151.application {
-    requires javafx.controls;
-    requires javafx.fxml;
-
-    requires com.dlsc.formsfx;
-
-    opens s25.cs151.application to javafx.fxml;
-    exports s25.cs151.application;
-}
